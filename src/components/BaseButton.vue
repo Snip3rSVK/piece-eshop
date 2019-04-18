1352c2ea04e9d37600d5fb931828ce88356c1a9d
Matej Pavlík <Snip3rSVK@gmail.com>
2019-04-19 00:18:51 +0200

    Day 1

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <router-link :to="to" class="base-button">
+    <slot></slot>
+  </router-link>
+</template>
+
+<script>
+  export default {
+    name: 'BaseButton',
+    props: {
+      to: String
+    }      
+  }
+</script>
+
+<style lang="scss" scoped>
+@import '~@/assets/sass/variables';
+
+$transition-length: 0.25s;
+
+.base-button {
+  box-sizing: border-box;
+  font-weight: 900;
+  font-size: 1.25em;
+  text-decoration: none;
+  border-radius: 100rem;
+  padding: 0.95em 2.05em;
+  //border: 4px solid $dark-brown;
+  box-shadow: 0 0 0 4px $dark-brown inset;
+  color: $dark-brown;
+  background: $golden-gradient;
+  transition: box-shadow $transition-length $ease-out-cubic;
+  outline: 0;
+}
+
+.base-button:hover, .base-button:focus {
+  box-shadow: 0 0 0 6px $dark-brown inset;
+  transition: box-shadow $transition-length $ease-out-cubic;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,82 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main class="home-content-wrapper">
+      <div class="home-content">
+        <div class="home-text-wrapper">
+          <h2 class="home-subtitle">Čokoládovňa</h2>
+          <h1 class="home-title">Piece</h1>
+        </div>
+        <base-button to="/eshop">Obchod</base-button>
+      </div>
+      <img class="home-logo" src="@/assets/img/logo.svg" alt="Logo Čokoládovňa Piece">
+    </main>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import BaseButton from '@/components/BaseButton';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
-  },
+    'base-button': BaseButton
+  }
 };
 </script>
+
+<style scoped lang="scss">
+@import '~@/assets/sass/variables';
+
+.home {
+  background-image: url('~@/assets/img/melting-chocolate.svg');
+  background-repeat: no-repeat;
+  background-position: top right;
+  background-size: auto 95%;
+  height: 100vh;
+  overflow: hidden;
+  user-select: none;
+}
+
+.home-content-wrapper {
+  position: relative;
+  margin: 0 auto;
+  width: 80%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+}
+
+.home-text-wrapper {
+  display: flex;
+  align-items: center;
+}
+
+.home-title {
+  font-size: 9.375em;
+  font-family: 'Pacifico', cursive;
+  font-weight: normal;
+  margin: 0;
+}
+
+.home-subtitle {
+  position: absolute;
+  font-size: 0.875em;
+  transform: translateX(-50%) rotate(-90deg);
+  transform-origin: center;
+  text-transform: uppercase;
+  border-bottom: 4px solid $dark-brown;
+  font-weight: 900;
+  letter-spacing: 5px;
+  padding: 0 0.71em 5px;
+  margin: 0;
+  //margin: 0 0 0 2rem;
+  display: inline-block;
+}
+
+.home-logo {
+  height: 4rem;
+  margin-top: 2rem;
+  align-self: flex-start;
+}
+</style>
